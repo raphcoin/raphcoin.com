6e19fab5ab7c5d72b61ec4e29d82fa66327fec81
enfipy <enfipy@gmail.com>
2019-07-01 16:14:06 +0000

    commit bed41bfddc35ce144161e90744e9770cb9711280 Author: David Ackerman <enfipy@gmail.com> Date:   Mon Jul 1 19:13:01 2019 +0300
        Fix camera move

--- sw.js
+++ sw.js
@@ -10,20 +10,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "06d515d8cce40415f00e29da1fdab96f"
   },
   {
+    "url": "/_nuxt/41bb58c51361fb555f2d.js",
+    "revision": "84fe2c5f5e39d56163cea8c2fa570036"
+  },
+  {
+    "url": "/_nuxt/46012a4c883b8ed5786b.js",
+    "revision": "28c61f0857a12e6c97811511d2aa62ac"
+  },
+  {
     "url": "/_nuxt/48bbed88895bae55c5cf.js",
     "revision": "ba790a9256fa62c4b59be1d634c1a822"
   },
   {
     "url": "/_nuxt/6ffa898317c873b8f31c.js",
     "revision": "ce2a9f5cf38f86f0772001f339489656"
-  },
-  {
-    "url": "/_nuxt/8d77fb5f9a9be7c1dbf5.js",
-    "revision": "05c5738cdb47906b56f4a26c721ae68d"
-  },
-  {
-    "url": "/_nuxt/d13cc37aa0b8a8ca6aad.js",
-    "revision": "5cfc61ffd2425f6127b4e4c8bfb7b290"
   },
   {
     "url": "/_nuxt/f74b448bd3a5b17198ae.js",
