619b3db12299d219047b5f980f34ba2fd3a54fc9
enfipy <enfipy@gmail.com>
2019-07-01 16:02:17 +0000

    commit 778caaad74168bba2c7dbe8e7bc9028c7e9838df Author: David Ackerman <enfipy@gmail.com> Date:   Mon Jul 1 19:01:16 2019 +0300
        Alter base route. Alter game camera

--- sw.js
+++ sw.js
@@ -6,28 +6,28 @@ workbox.precaching.precacheAndRoute([
     "revision": "f4f69a7d017bd996fc33c6830638b963"
   },
   {
-    "url": "/_nuxt/25e1c81efacfa681d03a.js",
-    "revision": "7631a5b5e1cb70584069a3c11ad23d70"
-  },
-  {
     "url": "/_nuxt/3e70807aea19c50386cf.js",
     "revision": "06d515d8cce40415f00e29da1fdab96f"
   },
   {
-    "url": "/_nuxt/69aadf5fb184688c50a8.js",
-    "revision": "ee27f0657268134c0049d3475f1d587a"
+    "url": "/_nuxt/48bbed88895bae55c5cf.js",
+    "revision": "ba790a9256fa62c4b59be1d634c1a822"
   },
   {
     "url": "/_nuxt/6ffa898317c873b8f31c.js",
     "revision": "ce2a9f5cf38f86f0772001f339489656"
   },
   {
-    "url": "/_nuxt/b6605dc3186f623f9b25.js",
-    "revision": "7acd623233bf6f19a9e65a11f55c386c"
+    "url": "/_nuxt/8d77fb5f9a9be7c1dbf5.js",
+    "revision": "05c5738cdb47906b56f4a26c721ae68d"
   },
   {
-    "url": "/_nuxt/f04feb17c8fe953b832c.js",
-    "revision": "12b80bf21a8cd048cf74a2a7a1942d00"
+    "url": "/_nuxt/d13cc37aa0b8a8ca6aad.js",
+    "revision": "5cfc61ffd2425f6127b4e4c8bfb7b290"
+  },
+  {
+    "url": "/_nuxt/f74b448bd3a5b17198ae.js",
+    "revision": "ce4d4b6733bfe1c20518a8888c76970f"
   }
 ], {
   "cacheId": "raphcoin",
