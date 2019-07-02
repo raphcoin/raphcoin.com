7602ebf19b89dd28c99fdf9c7dfdde448ae5ac63
enfipy <enfipy@gmail.com>
2019-07-02 09:59:34 +0000

    commit 54add6fbe7a5ab352c59f4fd1bdb6c3e3e551069 Author: David Ackerman <enfipy@gmail.com> Date:   Tue Jul 2 12:58:15 2019 +0300
        Add boot scene

--- sw.js
+++ sw.js
@@ -6,28 +6,28 @@ workbox.precaching.precacheAndRoute([
     "revision": "f4f69a7d017bd996fc33c6830638b963"
   },
   {
-    "url": "/_nuxt/318559666c5066a13f34.js",
-    "revision": "7fffb594077201e08df704e882bb12f2"
+    "url": "/_nuxt/0ae80841d7ed75812481.js",
+    "revision": "74e8aecd491c505b66e4a58f6092510a"
   },
   {
-    "url": "/_nuxt/904e3fda1cd06098be74.js",
-    "revision": "06ce8621af9ed78d144c36fa94cbfb00"
+    "url": "/_nuxt/3e248c0c56ecc47d9824.js",
+    "revision": "e7dc6da5683e2a77ce6daa57dadc9779"
   },
   {
-    "url": "/_nuxt/b4aa743a29d4407a6438.js",
-    "revision": "e1638cfb63e60704e22ddc39f52ff54a"
+    "url": "/_nuxt/3f06e3234a3fab5c2fbe.js",
+    "revision": "bccddc2555956019a146479808546636"
+  },
+  {
+    "url": "/_nuxt/40c22f98c7ce7bb8b004.js",
+    "revision": "ec293a3d6e0fb917e924a69086088ba7"
+  },
+  {
+    "url": "/_nuxt/6c3022a52afebb96f77c.js",
+    "revision": "9576945a6f4046133ea36eb0de10466d"
   },
   {
     "url": "/_nuxt/bd8d4699a4e7b741d698.js",
     "revision": "44fad8305aa2c2187dd37b2eb4e51d80"
-  },
-  {
-    "url": "/_nuxt/c37e509394089acbb99c.js",
-    "revision": "f0138d0029aea490b77c2bf0180b4da2"
-  },
-  {
-    "url": "/_nuxt/dbbdfabe6e41a4278789.js",
-    "revision": "28fede8aa36a319021ed89d79b611001"
   }
 ], {
   "cacheId": "raphcoin",
